9f217036d9885308b33e1ec5563665fb22e956f2
LMS <58144443+LMS5413@users.noreply.github.com>
2021-04-13 19:55:55 -0300

    kinnybot

--- src/eventos/Servidores/messageDelete.js
+++ src/eventos/Servidores/messageDelete.js
@@ -1,49 +1,6 @@
 const db = require('../../../db.js')
 const { MessageEmbed, WebhookClient } = require('discord.js')
 module.exports = async(client, message) => {
-    const mencao = message.mentions.members.first()
-    if (mencao) {
-        if (message.content.includes(`<@!${mencao.id}>`) || message.content.includes(`<@${mencao.id}>`)) {
-            if(mencao.user.bot) return;
-            let saifora = await db.webs.findOne({ id: message.author.id })
-            if (!saifora) {
-                    if(!message.channel.permissionsFor(client.user.id).has("MANAGE_WEBHOOKS")) return;
-                    await db.webs.create({ id: message.author.id, channelid: message.channel.id })
-                saifora = await db.webs.findOne({ id: message.author.id })
-                message.guild.channels.cache.get(message.channel.id).createWebhook(message.author.username, {
-                    avatar: message.author.displayAvatarURL({ format: 'png'}),
-                }).then(async webcreated => {
-                    await db.webs.findOneAndUpdate({ id: message.author.id}, { token: webcreated.token, idweb: webcreated.id, guildid: message.guild.id})
-
-                    saifora = await db.webs.findOne({ id: message.author.id })
-
-                    const webhookClient = new WebhookClient(saifora.idweb, saifora.token);
-                    webhookClient.send(message?.content.replace(`<@!${mencao.id}>`, mencao.user.username).replace(`<@${mencao.id}>`, mencao.user.username) || mencao.user.username, {
-                        username: message.author.username,
-                        avatarURL: message.author.displayAvatarURL({ format: 'png'}),
-                    }).then(async () => await webhookClient.delete())
-                    
-                })
-            } else {
-                await db.webs.findOneAndUpdate({ id: message.author.id, channelid: message.channel.id })
-                saifora = await db.webs.findOne({ id: message.author.id })
-                message.guild.channels.cache.get(message.channel.id).createWebhook(message.author.username, {
-                    avatar: message.author.displayAvatarURL({ format: 'png'}),
-                }).then(async webcreated => {
-                    await db.webs.findOneAndUpdate({ id: message.author.id}, { token: webcreated.token, idweb: webcreated.id,  })
-
-                    saifora = await db.webs.findOne({ id: message.author.id })
-
-                    const webhookClient = new WebhookClient(saifora.idweb, saifora.token);
-                    webhookClient.send(message?.content.replace(`<@!${mencao.id}>`, mencao.user.username).replace(`<@${mencao.id}>`, mencao.user.username) || mencao.user.username, {
-                        username: message.author.username,
-                        avatarURL: message.author.displayAvatarURL({ format: 'png'}),
-                    }).then(async () => await webhookClient.delete())
-                })
-                
-            }
-        }
-    }
         let guild = await db.idgr.findOne({grouplog: message.guild.id})
     if(!guild) return;
     if(!guild.logs.includes('deletes')) return;
